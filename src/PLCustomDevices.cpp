113919e6b02652fe2c23dd19ad057159adb5bb5d
Oğuz Kağan Yağlıoğlu <me@oguzkagan.xyz>
2022-12-20 00:11:24 +0300

    v0.2.0

--- src/PLCustomDevices.cpp
+++ src/PLCustomDevices.cpp
@@ -1,61 +1,75 @@
+//
+// Created by Oguz Kagan YAGLIOGLU
+// www.oguzkagan.xyz
+//
+
 #include <Arduino.h>
 #include <PLCustomDevices.h>
 
-PLCustomDevices::PLCustomDevices(WiFiClient *wifiClient, const char *mqttHost, uint16_t *port)
-{
+#include <utility>
+
+PLCustomDevices::PLCustomDevices(WiFiClient *wifiClient, const char *mqttHost, const uint16_t *port) {
     setMqttCallback(NULL);
-    PubSubClient tmqttClient(*wifiClient);
     this->host = mqttHost;
     this->port = port;
     this->wifiClient = wifiClient;
-    this->mqttClient = &tmqttClient;
+    this->mqttClient = new PubSubClient(*wifiClient);
 }
 
-PLCustomDevices::~PLCustomDevices()
-{
+PLCustomDevices::~PLCustomDevices() {
     this->mqttClient->~PubSubClient();
     this->doc.clear();
     free(responseBuffer);
 }
 
-void PLCustomDevices::init()
-{
+void PLCustomDevices::init() {
     mqttClient->setBufferSize(JSON_BUFFER);
     mqttClient->setServer(host, *port);
-    mqttClient->setCallback([this](char *topic, byte *payload, uint16_t length)
-                            { this->callback(topic, payload, length); });
+    mqttClient->setCallback(
+            [this](char *topic, byte *payload, uint16_t length) { this->callback(topic, payload, length); });
 }
 
-void PLCustomDevices::setAuthInfos(const char *deviceID, const char *mqttUsername, const char *mqttPass)
-{
+void PLCustomDevices::setAuthInfos(const char *deviceID, const char *mqttUsername, const char *mqttPass) {
     this->clientID = deviceID;
     this->username = mqttUsername;
     this->pass = mqttPass;
 }
 
-void PLCustomDevices::callback(char *topic, uint8_t *message, uint16_t length)
-{
+void PLCustomDevices::callback(char *topic, uint8_t *message, uint16_t length) {
     Serial.println("NEW MESSAGE");
-    DeserializationError error = deserializeJson(doc, message, length);
-    if (error)
-    {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char) message[i]);
+    }
+    doc.clear();
+    DeserializationError error = deserializeJson(doc, (const byte *) message, length);
+    if (error) {
         Serial.print(F("deserializeJson() failed: "));
         Serial.println(error.f_str());
         return;
     }
     commandAvailable = true;
-    if (customCallback)
-    {
+    if (customCallback_json) {
+        serializeJsonPretty(doc, Serial);
+        Serial.flush();
+        customCallback_json(doc);
+        if (!noAutoResponse) {
+            sendResponse();
+        }
+    }
+    if (customCallback) {
+        serializeJsonPretty(doc, Serial);
+        Serial.flush();
         customCallback(topic, message, length);
-        if (!noAutoResponse)
-        {
+        if (!noAutoResponse) {
             sendResponse();
         }
     }
 }
 
-bool PLCustomDevices::sendResponse()
-{
+bool PLCustomDevices::sendResponse() {
     commandAvailable = false;
     serializeJson(doc, responseBuffer);
     Serial.println(String(clientID) + "/o");
@@ -65,51 +79,43 @@ bool PLCustomDevices::sendResponse()
     return result;
 }
 
-PLCustomDevices &PLCustomDevices::setMqttCallback(CUSTOM_CALLBACK_SIGNATURE, bool noResponse)
-{
+PLCustomDevices &PLCustomDevices::setMqttCallback(CUSTOM_CALLBACK_SIGNATURE, bool noResponse) {
     this->noAutoResponse = noResponse;
-    this->customCallback = customCallback;
+    this->customCallback = std::move(customCallback);
     return *this;
 }
 
-void PLCustomDevices::connect(bool waitUntilConnect = false)
-{
+PLCustomDevices &PLCustomDevices::setJSONCallback(CUSTOM_CALLBACK_SIGNATURE_JSON, bool noResponse) {
+    this->noAutoResponse = noResponse;
+    this->customCallback_json = std::move(customCallback_json);
+    return *this;
+}
+
+void PLCustomDevices::connect(bool waitUntilConnect = false) {
     static unsigned long lastCall;
     static bool useDelay = false;
-    if (!waitUntilConnect && useDelay)
-    {
-        if (millis() - lastCall > 5000)
-        {
+    if (!waitUntilConnect && useDelay) {
+        if (millis() - lastCall > 5000) {
             useDelay = false;
-        }
-        else
-        {
+        } else {
             return;
         }
     }
-    do
-    {
-        if (!mqttClient->connected())
-        {
+    do {
+        if (!mqttClient->connected()) {
             Serial.print("Attempting MQTT connection...");
             Serial.printf("HOST: %s \nPORT: %u", host, *port);
-            if (mqttClient->connect(clientID, username, pass))
-            {
+            if (mqttClient->connect(clientID, username, pass)) {
                 Serial.println("connected");
                 mqttClient->subscribe((String(clientID) + "/i").c_str());
-            }
-            else
-            {
+            } else {
                 Serial.print("failed, rc=");
                 Serial.print(mqttClient->state());
                 Serial.println(" try again in 5 seconds");
                 // Wait 5 seconds before retrying
-                if (waitUntilConnect)
-                {
+                if (waitUntilConnect) {
                     delay(5000);
-                }
-                else
-                {
+                } else {
                     lastCall = millis();
                     useDelay = true;
                 }
@@ -118,14 +124,31 @@ void PLCustomDevices::connect(bool waitUntilConnect = false)
     } while (!mqttClient->connected() && waitUntilConnect);
 }
 
-void PLCustomDevices::loop()
-{
-    if (WiFi.status() == WL_CONNECTED)
-    {
+void PLCustomDevices::loop() {
+    if (WiFi.status() == WL_CONNECTED) {
         connect();
-        if (!mqttClient->loop())
-        {
+        if (!mqttClient->loop()) {
             mqttClient->connect(clientID, username, pass);
         }
     }
+}
+
+void PLCustomDevices::setUserID(const char *userID) {
+    this->userID = userID;
+}
+
+bool PLCustomDevices::sendSyncReq() {
+
+    if (!mqttClient->connected()) return false;
+
+    return mqttClient->publish(("dtx/" + String(userID) + "/" + String(clientID)).c_str(), "{\"synk\": true}");
+}
+
+bool PLCustomDevices::mqttConnected() {
+    return mqttClient->connected();
+}
+
+bool PLCustomDevices::mqttDisconnect() {
+    mqttClient->disconnect();
+    return !(mqttConnected());
 }
--- src/PLCustomDevices.h
+++ src/PLCustomDevices.h
@@ -1,44 +1,78 @@
+//
+// Created by Oguz Kagan YAGLIOGLU
+// www.oguzkagan.xyz
+//
+
 #ifndef JSON_BUFFER
 #define JSON_BUFFER 400
 #endif
 
 #ifndef PLCustomDevices_h
 #define PLCustomDevices_h
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+
 #if defined(ESP8266) || defined(ESP32)
+
 #include <functional>
+
 #define CUSTOM_CALLBACK_SIGNATURE std::function<void(char *, uint8_t *, unsigned int)> customCallback
+#define CUSTOM_CALLBACK_SIGNATURE_JSON std::function<void( JsonDocument& )> customCallback_json
 #else
 #define CUSTOM_CALLBACK_SIGNATURE void (*customCallback)(char *, uint8_t *, unsigned int)
+#define CUSTOM_CALLBACK_SIGNATURE_JSON void (*customCallback_json)( JsonDocument&)
 #endif
-class PLCustomDevices
-{
+
+
+class PLCustomDevices {
 private:
     CUSTOM_CALLBACK_SIGNATURE;
+    CUSTOM_CALLBACK_SIGNATURE_JSON;
     char responseBuffer[JSON_BUFFER];
     const char *host;
     const uint16_t *port;
+    const char *userID;
     const char *clientID;
     const char *username;
     const char *pass;
     WiFiClient *wifiClient = NULL;
     PubSubClient *mqttClient = NULL;
+
     void callback(char *topic, uint8_t *message, uint16_t length);
+
     bool noAutoResponse;
 
 public:
-    PLCustomDevices(WiFiClient *wifiClient, const char *mqttHost, uint16_t *port);
+    PLCustomDevices(WiFiClient *wifiClient, const char *mqttHost, const uint16_t *port);
+
+    void setUserID(const char *userID);
+
     void setAuthInfos(const char *deviceID, const char *mqttUsername, const char *mqttPass);
+
     void init();
+
     void connect(bool waitUntilConnect);
+
     void loop();
+
+    bool sendSyncReq();
+
     bool commandAvailable = false;
     StaticJsonDocument<JSON_BUFFER> doc;
+
     PLCustomDevices &setMqttCallback(CUSTOM_CALLBACK_SIGNATURE, bool noResponse = false);
+
+    PLCustomDevices &setJSONCallback(CUSTOM_CALLBACK_SIGNATURE_JSON, bool noResponse = false);
+
     bool sendResponse();
+
+    bool mqttConnected();
+
+    bool mqttDisconnect();
+
     ~PLCustomDevices();
 };
 
